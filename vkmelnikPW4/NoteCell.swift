ca2bf41e2deeb92aae168a15e60eec5c36d806c5
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-10-22 15:31:41 +0300

    Pressing 'add note' button opens 'create note' screen

---
+++ vkmelnikPW4/NoteCell.swift
@@ -0,0 +1,15 @@
+//
+//  NoteCell.swift
+//  vkmelnikPW4
+//
+//  Created by Vsevolod Melnik on 21.10.2021.
+//
+
+import UIKit
+
+class NoteCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+}
--- vkmelnikPW4/ViewController.swift
+++ vkmelnikPW4/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var emptyCollectionLabel: UILabel!
+    @IBOutlet weak var notesCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .add,
+                            target: self,
+                            action: #selector(createNote(sender:)))
+        
     }
 
-
+    @objc func createNote(sender: UIBarButtonItem) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier:"NoteViewController")
+        else {
+            return
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NoteCell", for: indexPath) as! NoteCell
+        cell.titleLabel.text = "Yeah"
+        cell.descriptionLabel.text = "That's great"
+        
+        return cell
+    }
+    
+}
